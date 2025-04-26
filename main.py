01e5701e4eb4e242ae3a385aad4da54b9ba5b611
Aleksandr <aleksandrkovalenko26330@gmail.com>
2025-04-26 16:38:17 +0300

    v 1.0

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def scan(target, ports):
     global open_port, close_port
     open_port = 0
     close_port = 0
-    print('\n' + f'Начало сканирование для цели {target}')
+    print(termcolor.colored(('\n' + f'Начало сканирование для цели {target}'), 'yellow'))
     for port in range(0, ports):
         scan_port(target, port)
 
@@ -16,13 +16,13 @@ def scan_port(ip_address, port):
     try:
         sock = socket.socket() # Инициируем объект socket для установления соединения
         sock.connect((ip_address, port)) # Устанавливаем соединение
-        print(f"[+] Порт {port} открыт")
+        print(termcolor.colored((f"[+] Порт {port} открыт"), 'green'))
         open_port += 1
         sock.close()
     except:
         close_port += 1
         if hide_close == 0:
-            print(f"[-] Порт {port} закрыт")
+            print(termcolor.colored((f"[-] Порт {port} закрыт"), 'red'))
         else:
             pass
 
@@ -34,15 +34,15 @@ close_port = 0
 hide_close = 0
 
 # Входные данные
-targets = input("[*] Введите цели для сканирования (через запятую): ")
-ports = int(input("[*] Введите количество портов для сканирования: "))
-hide_close = int(input("Скрывать ли закрытые порты (0 - нет, 1 - да): "))
+targets = input(termcolor.colored(("[*] Введите цели для сканирования (через запятую): "), 'blue'))
+ports = int(input(termcolor.colored(("[*] Введите количество портов для сканирования: "), 'blue')))
+hide_close = int(input(termcolor.colored(("Скрывать ли закрытые порты (0 - нет, 1 - да): "), 'blue')))
 # Определяем, одну или множество целей необходимо просканировать:
 if ',' in targets:
-    print("[*] Сканирование множества целей: ")
+    print(termcolor.colored(("[*] Сканирование множества целей: "), 'yellow'))
     for ip_addr in targets.split(','):  # Разделяем ip адреса целей и сканируем по очереди
         scan(ip_addr.strip(' '), ports)
-        print(f"Портов открыто: {open_port}, портов закрыто: {close_port}")
+        print(termcolor.colored((f"Портов открыто: {open_port}, портов закрыто: {close_port}"), 'yellow'))
 else:
     scan(targets, ports) # Иначе сканируем только одну цель
-    print(f"Портов открыто: {open_port}, портов закрыто: {close_port}")
+    print(termcolor.colored((f"Портов открыто: {open_port}, портов закрыто: {close_port}"), 'yellow'))
