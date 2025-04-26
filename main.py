8940d5a5321339cfc9d9ae690656672ec15e7c5d
Aleksandr <aleksandrkovalenko26330@gmail.com>
2025-04-26 15:00:08 +0300

    v 0.15

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 import termcolor
 
-# Инициируем объект socket для установления соедтнения
-sock = socket.socket()
-# Устанавливаем соединение
-sock.connect((ipaddress, port))
+# Пробуем подключиться к порту, если удалось то порт открыт, если нет - закрыт
+def scan_port(ip_address, port):
+    sock = socket.socket() # Инициируем объект socket для установления соединения
+    sock.connect((ip_address, port)) # Устанавливаем соединение
